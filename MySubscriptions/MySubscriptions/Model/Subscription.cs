93bed2cef4b86858c0a1f6fc50b07cc3f9868fd0
tylercenac <tycenac@gmail.com>
2020-03-26 02:06:20 -0500

    Configured Cloud Firestore and dependency service interfaces

---
+++ MySubscriptions/MySubscriptions/Model/Subscription.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySubscriptions.Model
+{
+    public class Subscription
+    {
+
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public DateTime SubscribedDate { get; set; }
+        public bool IsActive { get; set; }
+
+        public Subscription()
+        {
+
+        }
+
+    }
+}
---
+++ MySubscriptions/MySubscriptions/ViewModel/Helpers/DatabaseHelper.cs
@@ -0,0 +1,43 @@
+﻿using MySubscriptions.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySubscriptions.ViewModel.Helpers
+{
+
+    public interface IFirestore
+    {
+        Task<bool> InsertSubscription(Subscription subscription);
+        Task<bool> DeleteSubscription(Subscription subscription);
+        Task<bool> UpdateSubscription(Subscription subscription);
+        Task<IList<Subscription>> ReadSubscription();
+    }
+
+    public class DatabaseHelper
+    {
+
+        private static IFirestore firestore;
+
+        public Task<bool> DeleteSubscription(Subscription subscription)
+        {
+            return firestore.DeleteSubscription(subscription);
+        }
+
+        public Task<bool> InsertSubscription(Subscription subscription)
+        {
+            return firestore.InsertSubscription(subscription);
+        }
+
+        public Task<IList<Subscription>> ReadSubscriptions()
+        {
+            return firestore.ReadSubscription();
+        }
+
+        public Task<bool> UpdateSubscription(Subscription subscription)
+        {
+            return firestore.UpdateSubscription(subscription);
+        }
+    }
+}
